33efb404de086719b6c27035e8f5a916331afa70
Brénainn Woodsend <bwoodsend@gmail.com>
2022-09-20 20:59:32 +0100

    Fixup/annotate/test CachedMirror().

--- polycotylus/_mirror.py
+++ polycotylus/_mirror.py
@@ -6,6 +6,7 @@ from http.client import HTTPResponse
 import shutil
 import re
 import threading
+import time
 from functools import wraps
 from fnmatch import fnmatch
 from pathlib import Path
@@ -18,9 +19,36 @@ cache_root = Path(appdirs.user_cache_dir("polycotylus"))
 
 
 class CachedMirror:
+    """A mirror of a Linux distribution's package repository which caches
+    downloads persistently.
+    """
 
     def __init__(self, base_url, base_dir, index_patterns, ignore_patterns,
                  port, install, last_sync_time):
+        """
+        Args:
+            base_url:
+                The URL of the root of the repository index. This can usually be
+                found in an /etc/{package manager}/mirrors or similar file.
+            base_dir:
+                A persistent directory to store cached files in.
+            index_patterns:
+                A list of globs which indicate repository index files.
+            ignore_patterns:
+                A list of globs which indicate files which should always return
+                404 errors. This can usually be left empty.
+            port:
+                An integer port number. Any number not already used will do.
+            install:
+                A shell command which replaces the distribution's list of
+                mirrors with this mirror.
+            last_sync_time:
+                A function, taking this CachedMirror() instance as an argument,
+                which returns an integer timestamp corresponding to the time
+                the repository was last updated. This is typically stored in a
+                plain text fail at ${base_url}/last-sync.
+
+        """
         self.base_url = base_url.strip("/")
         self.base_dir = Path(base_dir)
         self.index_patterns = index_patterns
@@ -30,22 +58,25 @@ class CachedMirror:
         self._lock = threading.Lock()
         self._listeners = 0
         self.last_sync_time = lambda: last_sync_time(self)
+        self._in_progress = set()
 
     def serve(self):
-        handler = type("Handler", (RequestHandler,), {"parent": self})
-
-        httpd = ThreadingHTTPServer(("", self.port), handler)
-        host, port = httpd.socket.getsockname()
-        host = "localhost" if os.name == "nt" else host
-        print("http://{}:{}".format(host, self.port))
-        print(f"Install via:\n{self.install}")
-        try:
-            httpd.serve_forever()
-        except:
-            httpd.shutdown()
+        """Enable this mirror and block until killed (via Ctrl+C)."""
+        with self:
+            host = "localhost" if os.name == "nt" else "0.0.0.0"
+            print("http://{}:{}".format(host, self.port))
+            print(f"Install via:\n{self.install}")
+            try:
+                while True:
+                    time.sleep(1)
+            except KeyboardInterrupt:
+                pass
 
     def __enter__(self):
         with self._lock:
+            # If multiple enters, avoid port competition by ensuring that there
+            # is only one server. Keep a counter of how many
+            # __enter__()/__exit__() there have been.
             if self._listeners:
                 self._listeners += 1
                 return
@@ -61,10 +92,16 @@ class CachedMirror:
             self._listeners -= 1
             if self._listeners:
                 return
+        # Wait until all running downloads are complete to avoid competing over
+        # ports if this mirror is re-enabled soon after.
+        while self._in_progress:
+            time.sleep(.1)
         self._httpd.shutdown()
         del self._httpd
 
     def decorate(self, f):
+        """@decorate a function requiring the mirror set up so that this mirror
+        is started/stopped whenever the function is called/exits."""
 
         @wraps(f)
         def wrapped(*args, **kwargs):
@@ -75,6 +112,7 @@ class CachedMirror:
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    """Handle a single request from a package manager."""
     parent: CachedMirror
 
     def do_HEAD(self):
@@ -84,18 +122,43 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
     def _head(self):
+        """Triage an incoming request for a file.
+
+        Returns:
+            Either:
+            - An open connection to the upstream repository if the request makes
+              sense and isn't already cached.
+            - A path to the local cache if this file is cache-able and already
+              downloaded.
+            - None if there are any errors.
+
+        """
         path = self.cache
         if any(fnmatch(self.path, i) for i in self.parent.ignore_patterns):
             self.send_response(404)
             return
-        if path.is_file():
-            self.send_response(HTTPStatus.OK)
-            return path
         if path.is_dir():
             self.send_response(404)
             return
+        # File is cached - send the cache.
+        if self.path in self.parent._in_progress or path.is_file():
+            while self.path in self.parent._in_progress:
+                # If the file is being downloaded in another request then we
+                # have to wait for it to finish before it's safe to read the
+                # cache. Keep sending little "it's coming" messages so that the
+                # client package manager doesn't time out.
+                self.send_response(HTTPStatus.ACCEPTED)
+                time.sleep(.1)
+            self.send_response(HTTPStatus.OK)
+            return path
         try:
+            # File is not cached - attempt to download it.
             response = urlopen(self.parent.base_url + self.path)
+            if response.headers["Content-Type"] == "text/html":
+                # Crude block from downloading .../index.html files.
+                self.send_response(404)
+                response.close()
+                return
             self.send_response(HTTPStatus.OK)
             return response
         except HTTPError as ex:
@@ -117,6 +180,8 @@ class RequestHandler(BaseHTTPRequestHandler):
                 timestamp = cache.stat().st_mtime
                 if any(timestamp < i for i in self.parent.last_sync_time()):
                     response = urlopen(self.parent.base_url + self.path)
+                else:
+                    os.utime(cache)
 
         if isinstance(response, Path):
             self._cache_send()
@@ -124,6 +189,7 @@ class RequestHandler(BaseHTTPRequestHandler):
             self._download_send(response)
 
     def _cache_send(self):
+        """Send a file from cache to the client."""
         cache = self.cache
         if range := re.match(r"bytes=(\d*)-(\d*)", self.headers["Range"] or ""):
             start = int(range[1] or 0)
@@ -136,6 +202,9 @@ class RequestHandler(BaseHTTPRequestHandler):
             shutil.copyfileobj(f, self.wfile, length)
 
     def _download_send(self, response):
+        """Send a file from the upstream package repository to the client
+        package manager and to cache."""
+        self.parent._in_progress.add(self.path)
         cache = self.cache
         cache.parent.mkdir(parents=True, exist_ok=True)
         try:
@@ -146,17 +215,23 @@ class RequestHandler(BaseHTTPRequestHandler):
                         f.write(buffer[:consumed])
                         try:
                             self.wfile.write(buffer[:consumed])
-                        except BrokenPipeError:
+                        except (BrokenPipeError, ConnectionResetError):
+                            # If the client package manager cancels half way
+                            # through then continue the download to cache since
+                            # the client will likely ask for the rest of the
+                            # file soon after.
                             shutil.copyfileobj(response, f)
                             return
-        except HTTPError as ex:
-            self.send_response(ex.code)
-        except:
-            pass
-        else:
-            return
-        if cache.exists():
-            os.remove(cache)
+        except Exception as ex:  # pragma: no cover
+            if isinstance(ex, HTTPError):
+                self.send_response(ex.code)
+            else:
+                self.send_response(HTTPStatus.INTERNAL_SERVER_ERROR)
+            if cache.exists():
+                os.remove(cache)
+            raise
+        finally:
+            self.parent._in_progress.remove(self.path)
 
 
 def _arch_sync_time(self):
@@ -196,4 +271,9 @@ mirrors = {
 }
 
 if __name__ == "__main__":
-    mirrors[sys.argv[1]].serve()
+    if len(sys.argv) > 2:
+        import subprocess
+        with mirrors[sys.argv[1]]:
+            subprocess.run(sys.argv[2:])
+    else:
+        mirrors[sys.argv[1]].serve()
---
+++ tests/test_mirror.py
@@ -0,0 +1,166 @@
+import gzip
+import time
+from urllib.request import urlopen, Request
+from urllib.error import URLError, HTTPError
+import threading
+import signal
+import os
+
+from docker import from_env
+import pytest
+
+from polycotylus._mirror import mirrors, CachedMirror, _alpine_sync_time
+
+
+def _alpine_mirror(tmp_path):
+    _alpine = mirrors["alpine"]
+    return CachedMirror(
+        _alpine.base_url,
+        tmp_path,
+        _alpine.index_patterns,
+        ["*.penguin"],
+        9989,
+        _alpine.install.replace("8901", "9989"),
+        _alpine_sync_time,
+    )
+
+
+def test_basic(tmp_path):
+    self = _alpine_mirror(tmp_path)
+    url = "http://0.0.0.0:9989/MIRRORS.txt"
+
+    with pytest.raises(URLError):
+        urlopen(url).close()
+
+    with self:
+        with urlopen(url) as response:
+            content = response.read()
+        cache = tmp_path / "MIRRORS.txt"
+        assert cache.read_bytes() == content
+        mtime = cache.stat().st_mtime
+        with self:
+            with self:
+                with urlopen(url) as response:
+                    assert response.read() == content
+    assert cache.stat().st_mtime == mtime
+
+    contents = []
+
+    def _test():
+        time.sleep(.2)
+        try:
+            with urlopen(url) as response:
+                contents.append(response.read())
+        finally:
+            signal.raise_signal(signal.SIGINT)
+
+    thread = threading.Thread(target=_test)
+    thread.start()
+    self.serve()
+    assert contents[0] == content
+
+    with pytest.raises(URLError):
+        urlopen(url).close()
+
+    @self.decorate
+    def _fetch(url):
+        with urlopen(url) as response:
+            return response.read()
+
+    assert _fetch(url) == content
+
+
+def test_index_caching(tmp_path, monkeypatch):
+    self = _alpine_mirror(tmp_path)
+    with self:
+        non_index = "MIRRORS.txt"
+        index = "edge/main/x86_64/APKINDEX.tar.gz"
+
+        now = time.time()
+        monkeypatch.setattr(time, "time", lambda: now)
+
+        for file in (non_index, index):
+            with urlopen("http://0.0.0.0:9989/" + file) as response:
+                response.read()
+            assert (tmp_path / file).exists()
+            (tmp_path / file).write_bytes(b"dummy value")
+            with urlopen("http://0.0.0.0:9989/" + file) as response:
+                assert response.read() == b"dummy value"
+            os.utime(tmp_path / file, (time.time() - 1_000_000,) * 2)
+
+        with urlopen("http://0.0.0.0:9989/" + non_index) as response:
+            assert response.read() == b"dummy value"
+        with urlopen("http://0.0.0.0:9989/" + index) as response:
+            assert response.read() != b"dummy value"
+
+
+def test_head(tmp_path):
+    self = _alpine_mirror(tmp_path)
+    url = "http://0.0.0.0:9989/MIRRORS.txt"
+
+    with self:
+        with urlopen(Request(url, method="HEAD")) as response:
+            assert not response.read()
+        cache = tmp_path / "MIRRORS.txt"
+        assert not cache.exists()
+
+        with urlopen(url) as response:
+            assert response.read()
+        with urlopen(Request(url, method="HEAD")) as response:
+            assert not response.read()
+
+        with pytest.raises(URLError):
+            urlopen(Request(url + "cake", method="HEAD")).close()
+
+
+@pytest.mark.parametrize("path", [
+    "/cake/sausages", "/..", "//etc", "/edge/main/x86_64/",
+    "/edge/main/x86_64/aaudit-0.7.2-r3.apk.penguin"
+])
+def test_errors(tmp_path, path):
+    self = _alpine_mirror(tmp_path)
+
+    with self:
+        with pytest.raises(HTTPError) as error:
+            urlopen("http://0.0.0.0:9989" + path)
+        assert error.value.code == 404
+
+
+def test_concurrent(tmp_path):
+    self = _alpine_mirror(tmp_path)
+    url = "http://0.0.0.0:9989/edge/main/x86_64/APKINDEX.tar.gz"
+
+    with self:
+        for i in range(3):
+            with urlopen(url) as a:
+                part = a.read(100)
+                with urlopen(url) as b:
+                    raw = b.read()
+                    gzip.decompress(raw)
+                gzip.decompress(part + a.read())
+
+
+def test_kill_resume(tmp_path):
+    self = _alpine_mirror(tmp_path)
+    url = "http://0.0.0.0:9989/edge/main/x86_64/APKINDEX.tar.gz"
+    with self:
+        with urlopen(url) as response:
+            chunk = response.read(3233)
+
+    with self:
+        with urlopen(Request(url,
+                             headers={"Range": "bytes=3233-"})) as response:
+            content = chunk + response.read()
+
+    gzip.decompress(content)
+
+
+def test_tar_integrity(tmp_path):
+    docker = from_env()
+    self = _alpine_mirror(tmp_path)
+
+    for i in range(3):
+        with self:
+            docker.containers.run(
+                "alpine", ["ash", "-c", f"{self.install} && apk add libbz2"],
+                network_mode="host")
