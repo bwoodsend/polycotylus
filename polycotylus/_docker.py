e081ff577fc59ff597073627e5f3e1ffc0ad0c58
Brénainn Woodsend <bwoodsend@gmail.com>
2022-10-21 00:38:50 +0100

    wip

--- polycotylus/_docker.py
+++ polycotylus/_docker.py
@@ -24,7 +24,7 @@ class run:
             arguments += command
         if verbosity >= 1:
             human_friendly = "$ docker run --rm " + shlex.join(arguments)
-            print(human_friendly, flush=True)
+            Logger(verbosity).command(human_friendly)
 
         p = _run(["docker", "create"] + arguments, stdout=PIPE)
         self.id = p.stdout.decode().splitlines()[-1]
@@ -62,8 +62,7 @@ def _tee_run(command, verbosity=0, **kwargs):
         chunks = []
         while (chunk := p.stdout.readline()) or p.poll() is None:
             chunks.append(chunk)
-            if verbosity >= 2 :
-                sys.stdout.write(chunk)
+            Logger(verbosity).output(chunk)
     return p.returncode, "".join(chunks)
 
 
@@ -71,8 +70,7 @@ def build(dockerfile, root, target=None, verbosity=0):
     command = ["docker", "build", "-f", str(dockerfile), "--network=host", "."]
     if target:
         command += ["--target", target]
-    if verbosity >= 1:
-        print("$", shlex.join(command))
+    Logger(verbosity).command(shlex.join(command))
     returncode, output = _tee_run(command, verbosity, cwd=root)
     if returncode:
         raise Error("$ " + shlex.join(command), output)
@@ -89,3 +87,73 @@ class Error(Exception):
     def __str__(self):
         return f"Docker command:\n    {self.command}\n" \
             "returned an error:\n" + textwrap.indent(self.output, "    ")
+
+
+class Logger:
+    def __init__(self, level=0):
+        self.level = level
+
+    def command(self, *message):
+        if self.level >= 1:
+            print(*message, flush=True)
+        self.any()
+
+    def output(self, *message):
+        if self.level >= 2:
+            print(*message, end="")
+        self.any()
+
+    def any(self):
+        if self.level >= 3:
+            import random
+            import time
+            import uuid
+            print(time.ctime(), uuid.uuid1(), "INFO", random.choice([
+                "Executing logging command",
+                "Processing something",
+                "Wasting screen space",
+                "Entering active method",
+                "Thinking",
+                "Executing code",
+                "Proceeding with current step",
+                "Misc logging statement",
+                "Choosing next logging message"
+                "Running procedural process whose procedure is being processed",
+                "Constructing function arguments",
+                "Adding two integers together",
+                "Setting current status to \"logging\"",
+                "Writing to console stdout",
+                "Formatting string",
+                "Piffle wiffle waffle poff",
+            ]), sep=" :: ")
+        if self.level >= 4:
+            print(pink_flying_unicorn)
+
+
+pink_flying_unicorn = "\x1b[35m" r"""\
+                      . . . .
+                      ,`,`,`,`,
+. . . .               `\`\`\`\;
+`\`\`\`\`,            ~|;!;!;\!
+ ~\;\;\;\|\          (--,!!!~`!       .
+(--,\\\===~\         (--,|||~`!     ./
+ (--,\\\===~\         `,-,~,=,:. _,//
+  (--,\\\==~`\        ~-=~-.---|\;/J,
+   (--,\\\((```==.    ~'`~/       a |
+     (-,.\\('('(`\\.  ~'=~|     \_.  \
+        (,--(,(,(,'\\. ~'=|       \\_;>
+          (,-( ,(,(,;\\ ~=/        \
+          (,-/ (.(.(,;\\,/          )
+           (,--/,;,;,;,\\         ./------.
+             (==,-;-'`;'         /_,----`. \
+     ,.--_,__.-'                    `--.  ` \
+    (='~-_,--/        ,       ,!,___--. \  \_)
+   (-/~(     |         \   ,_-         | ) /_|
+   (~/((\    )\._,      |-'         _,/ /
+    \\))))  /   ./~.    |           \_\;
+ ,__/////  /   /    )  /
+  '===~'   |  |    (, <.
+           / /       \. \
+         _/ /          \_\
+        /_!/            >_\
+""" "\x1b[0m"
