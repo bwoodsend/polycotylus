364f56bd458190e2c96f232df6f7d0b63395d995
Brénainn Woodsend <bwoodsend@gmail.com>
2022-11-06 19:50:26 +0000

    Refactor long string writing for error messages.

--- polycotylus/_exceptions.py
+++ polycotylus/_exceptions.py
@@ -1,3 +1,12 @@
+import textwrap
+import re
+
+
+def _unravel(text):
+    return re.sub("(^ .*\n)|\n(?! )", lambda m: m[1] or " ",
+                  textwrap.dedent(text.lstrip("\n")), flags=re.M)
+
+
 class PolycotylusUsageError(Exception):
     pass
 
@@ -9,10 +18,12 @@ class InvalidLocale(PolycotylusUsageError):
         self.location = location
 
     def __str__(self):
-        return f"Locale '{self.locale}', specified at {self.location}, is " \
-            "not a valid locale identifier. Run\n     " \
-            'python -c "import locale; print(*locale.locale_alias)"\n' \
-            "to see a list of valid locales."
+        return _unravel(f"""
+            Locale '{self.locale}', specified at {self.location}, is not a valid
+            locale identifier. Run:
+                python -c "import locale; print(*locale.locale_alias)"
+            to see a list of valid locales.
+        """)
 
 
 class InvalidMimetypePattern(PolycotylusUsageError):
@@ -22,10 +33,12 @@ class InvalidMimetypePattern(PolycotylusUsageError):
         self.location = location
 
     def __str__(self):
-        return f"Mimetype '{self.mimetype}', specified at {self.location}, " \
-            "does not match any known mimetypes. Run\n     " \
-            'python -c "import mimetypes; print(*mimetypes.types_map.values())"\n' \
-            "to see a list of valid mimetypes."
+        return _unravel(f"""
+            Mimetype '{self.mimetype}', specified at {self.location}, does not
+            match any known mimetypes. Run:
+                python -c "import mimetypes; print(*mimetypes.types_map.values())"
+            to see a list of valid mimetypes.
+        """)
 
 
 class PolycotylusYAMLParseError(PolycotylusUsageError):
