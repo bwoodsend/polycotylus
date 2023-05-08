c1b5cdcc08ff0079a25f0d2db6f05f8463bc804b
Brénainn Woodsend <bwoodsend@gmail.com>
2023-05-08 17:09:31 +0100

    Fix -r requirements.txt in dependencies.run.pip and dependencies.build.pip.

--- tests/mock-packages/silly-name/sIlLy_Nam3.py
+++ tests/mock-packages/silly-name/sIlLy_Nam3.py
@@ -3,6 +3,7 @@ import tkinter
 
 import tabulate
 import certifi
+import appdirs
 
 a = "𝜸"
 assert shutil.which("jq")
--- tests/mock-packages/silly-name/setup.py
+++ tests/mock-packages/silly-name/setup.py
@@ -4,6 +4,7 @@ import sqlite3
 import pyperclip
 import colorama
 import setuptools
+import webencodings
 
 assert shutil.which("croc")
 
