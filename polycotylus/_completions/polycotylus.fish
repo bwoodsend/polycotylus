a334806227250973f550f62dc589a495320d49c6
Brénainn Woodsend <bwoodsend@gmail.com>
2023-07-09 17:26:18 +0100

    Assorted fish completion improvements.
    * Add -h/--help.
    * Prevent filename completion (polycotylus's CLI never references files)
    * Prevent atomic options (--list-localizations/--completion) from being
      suggested when a Linux distribution is already chosen.

--- polycotylus/_completions/polycotylus.fish
+++ polycotylus/_completions/polycotylus.fish
@@ -1,4 +1,5 @@
 set -l distributions alpine arch fedora manjaro opensuse void
+complete -c polycotylus -f
 complete -x -c polycotylus -n "not __fish_seen_subcommand_from $distributions" -a "$distributions"
 
 complete -c polycotylus -x -l architecture -n "not __fish_seen_subcommand_from architecture $distributions" -a 'aarch64 armv7 ppc64le x86 x86_64'
@@ -11,6 +12,7 @@ complete -c polycotylus -x -l architecture -n "__fish_seen_subcommand_from void"
 
 complete -c polycotylus -f -s q -l quiet -d 'Decrease verbosity'
 complete -c polycotylus -f -l post-mortem -d 'Enter container on error'
+complete -c polycotylus -f -s h -l help -d 'Show help'
 
-complete -c polycotylus -x -l completion -a 'fish'
-complete -c polycotylus -x -l list-localizations -a 'language region modifier'
+complete -c polycotylus -x -l completion -n "not __fish_seen_subcommand_from $distributions" -a 'fish' -d 'Generate shell completions'
+complete -c polycotylus -x -l list-localizations -n "not __fish_seen_subcommand_from $distributions" -a 'language region modifier'
