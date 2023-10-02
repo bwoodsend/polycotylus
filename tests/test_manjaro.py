9f6eb952b1826499a759c73c1a7819c32f8a74dd
Brénainn Woodsend <bwoodsend@gmail.com>
2023-10-03 00:00:23 +0100

    Fix Alpine 3.17's mirror not installing.
    And extend the python_extras test so that it checks files are indeed
    going through the mirror.

--- tests/test_manjaro.py
+++ tests/test_manjaro.py
@@ -3,13 +3,12 @@ import contextlib
 from polycotylus._project import Project
 from polycotylus._manjaro import Manjaro
 from polycotylus import _docker
-from polycotylus._mirror import cache_root, _manjaro_preferred_mirror
+from polycotylus._mirror import cache_root, _manjaro_preferred_mirror, RequestHandler
 import shared
 
 
 class TestCommon(shared.Base):
     cls = Manjaro
-    base_image = "manjarolinux/base"
     package_install = "pacman -Sy --needed --noconfirm glibc"
 
 
@@ -19,15 +18,20 @@ def test_dumb_text_viewer():
     self.test(self.build()["main"])
 
 
-def test_mirror_detection():
+def test_mirror_detection(monkeypatch):
     with contextlib.suppress(FileNotFoundError):
         (cache_root / "manjaro-mirror").unlink()
+    requests = []
+    original_do_GET = RequestHandler.do_GET
+    monkeypatch.setattr(RequestHandler, "do_GET",
+                        lambda self: requests.append(self.path) or original_do_GET(self))
     with Manjaro.mirror:
         for architecture in ["x86_64", "aarch64"]:
             _docker.run("manjarolinux/base", f"""
                 {Manjaro.mirror.install}
                 pacman -Sy
             """, architecture=architecture, tty=True)
+    assert requests, "Mirror is being ignored"
     assert _manjaro_preferred_mirror() == _manjaro_preferred_mirror()
 
 
